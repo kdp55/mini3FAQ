820af66653fb0eae7b03e3c0bdcab6857028d223
kaksha96 <kdp55@njit.edu>
2018-11-27 11:53:45 -0500

    Fixed: Added Question controller and question.blade.php to view questions and answers as per particular user profile

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,8 @@ Route::get('/questions/{question_id}/answers/{answer_id}/edit', 'AnswerControlle
 Route::post('/questions/{question_id}/answers/', 'AnswerController@store')->name('answers.store');
 Route::patch('/questions/{question_id}/answer/{answer_id}', 'AnswerController@update')->name('answers.update');
 Route::delete('/questions/{question_id}/answer/{answer_id}', 'AnswerController@destroy')->name('answers.destroy');
+
+
 Route::resources([
     'questions' => 'QuestionController',
 ]);
